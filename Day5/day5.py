6cc4888d0796acca8d6de36805892299076588f0
EuanRobertson1 <92741298+EuanRobertson1@users.noreply.github.com>
2022-12-05 14:19:17 +0000

    lol

---
+++ Day5/day5.py
@@ -0,0 +1,20 @@
+import array
+def readFromFile():
+    #open file
+    file = open("Day4/input.txt")
+    #get contents
+    con = file.readlines()
+    #close fle
+    file.close()
+    return con
+
+def fillArray(c):
+    i = 0
+    while(c[i] != "\n"):
+        
+
+    
+
+def main():
+    content = readFromFile()
+    stacks = fillArray(content)
